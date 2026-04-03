cc34e33507ec269eac035a30c9494e2861db8c2d
aleclevy <aleclevy3@gmail.com>
2026-04-02 18:43:19 -0700

    almost done

--- Lab1/asgn1.m
+++ Lab1/asgn1.m
@@ -1,7 +1,8 @@
 %q1
 x = linspace(1,100);
 y = x(2:2:end);
-
+display(x);
+display(y);
 %q2
 row = 1;
 col = 1;
@@ -14,6 +15,47 @@ for t = 0.0:0.1:0.4
     end
     col = col + 1;
 end
+display(final);
 
 %q3
-while()
+t = 0;
+while((exp(1.2) *cos(35* t) < 10) && (t^3 < 10) )
+    t=t+.01;
+end
+fprintf('w=30: %f\n',t);
+t=0;
+while((exp(1.2) *cos(40* t) < 10) && (t^3 < 10) )
+    t=t+.01;
+end
+fprintf('w=40: %f\n',t)
+t=0;
+while((exp(1.2) *cos(45* t) < 10) && (t^3 < 10) )
+    t=t+.01;
+end
+fprintf('w=45: %f\n', t);
+
+%q4
+result4 = [];
+for(t=linspace(0,1,15))
+    result4(end+1) = (4 * cos((2 * pi * t) + 0.2)) + (3 * sin((pi^2) * t));
+end
+display(result4);
+
+%q5
+f1 = 0.2;
+f2 = 0.425;
+t = 0:0.1:10;
+
+s1 = sin(2 * pi * f1 * t);
+s2 = sin((2 * pi * f2 * t) + 0.4);
+s3 = s1 + s2;
+
+plot(t, s1);
+hold on
+plot(t, s2);
+hold on
+plot(t, s3);
+ylabel('s (1, 2, 3)');
+xlabel('t');
+
+hold off
